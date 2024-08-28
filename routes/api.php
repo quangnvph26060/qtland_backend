0df8f6c48b771a3ac465de556ada1db3bb7788f9
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-08-28 11:42:37 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/update-login-status', [AuthController::class, 'updateLoginStatus']);
 // Route for authentication
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/login', [AuthController::class, 'login']);
